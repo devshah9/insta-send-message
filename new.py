e7d2477b3ca0ab1a3cb908e5f7362e541335786f
dev <devshah972@gmail.com>
2020-12-06 23:23:42 +0530

    updated

--- new.py
+++ new.py
@@ -4,10 +4,6 @@ from selenium.webdriver.common.by import By  # for locating elements
 from selenium.webdriver.support import expected_conditions as ec  # condition that wait for element
 from selenium.webdriver.support.ui import WebDriverWait  # for wait if condition is not fulfilled
 
-# python
-
-
-import time  # for waiting
 
 print('Watch the details you enter are correct, else this program will fail')
 username_input = input('type your username >> ')
@@ -54,6 +50,7 @@ element.click()
 # for loading the screen
 element = wait.until(ec.element_to_be_clickable((By.XPATH, '/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/button[2]')))
 
+# Will run until the number of times_of_message is sended
 i = 0
 while i != times_of_message:
     i += 1
